422dc1fcf538c54e94d94b08d1f472885fe44a41
bamboosjtu <bamboopengkezhu@gmail.com>
2020-02-28 01:41:37 +0800

    开放题

--- openwork.py
+++ openwork.py
@@ -3,9 +3,15 @@
 import torch, torchtext
 import math
 import time
+import random
+import matplotlib.pyplot as plt
+import sys
+import os
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+savedStdout = sys.stdout
+
 
 # 建立字符索引
 with open("Datasets/jaychou_lyrics.txt", encoding="utf-8") as f:
@@ -24,18 +30,32 @@ def perplexity(l_sum, n):
 
 # utils
 def one_hot(x, n_class, dtype=torch.float32):
-    result = torch.zeros(x.shape[0], n_class, dtype=dtype, device=x.device)  # shape: (n, n_class)
-    result.scatter_(1, x.long().view(-1, 1), 1)  # result[i, x[i, 0]] = 1
+    """
+    input shape :  (n,)
+    output shape:  (n, n+class)
+    """
+    result = torch.zeros(x.shape[0], n_class, dtype=dtype, device=x.device)
+    result.scatter_(1, x.long().view(-1, 1), 1)
     return result
 
+
 def to_onehot(X, n_class):
+    """
+    input shape :  (batch_size, num_steps)
+    output shape:  num_steps x (batch_size, n_class)
+    """
     return [one_hot(X[:, i], n_class) for i in range(X.shape[1])]
 
+
 def data_iter_consecutive(corpus_indices, batch_size, num_steps, device=None):
+    """
+    X shape:   (batch_size, num_steps)
+    Y shape:   (batch_size, num_steps)
+    """
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    corpus_len = len(corpus_indices) // batch_size * batch_size  # 保留下来的序列的长度
-    corpus_indices = corpus_indices[: corpus_len]  # 仅保留前corpus_len个字符
+    corpus_len = len(corpus_indices) // batch_size * batch_size
+    corpus_indices = corpus_indices[: corpus_len]
     indices = torch.tensor(corpus_indices, device=device)
     indices = indices.view(batch_size, -1)  # resize成(batch_size, )
     batch_num = (indices.shape[1] - 1) // num_steps
@@ -45,7 +65,10 @@ def data_iter_consecutive(corpus_indices, batch_size, num_steps, device=None):
         Y = indices[:, i + 1: i + num_steps + 1]
         yield X, Y
 
-def grad_clipping(params, theta, device):
+
+def grad_clipping(params, theta, device=None):
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     norm = torch.tensor([0.0], device=device)
     for param in params:
         norm += (param.grad.data ** 2).sum()
@@ -55,38 +78,55 @@ def grad_clipping(params, theta, device):
             param.grad.data *= (theta / norm)
 
 
-num_steps, batch_size = 35, 2
 # RNN
 class RNNModel(torch.nn.Module):
-    def __init__(self, vocab_size, num_hiddens=256):
+    """
+    RNN input shape   : (num_steps, batch_size, vocab_size) if batch_first=False
+    RNN output shape  : (num_steps, batch_size, vocab_size) if batch_first=False
+    RNN hiddens shape : (num_steps, batch_size, hidden_size) if batch_first=False
+    state shape       : (num_layers * num_directions, batch_size, vocab_size)
+    dense output shape: (num_steps * batch_size, vocab_size)
+    """
+    def __init__(self, rnn_layer):
         super(RNNModel, self).__init__()
-        self.rnn = torch.nn.RNN(input_size=vocab_size, hidden_size=num_hiddens)
-        self.vocab_size = vocab_size
-        self.hidden_size = self.rnn.hidden_size
+        self.rnn = rnn_layer
+        self.vocab_size = rnn_layer.input_size
+        self.hidden_size = rnn_layer.hidden_size * (2 if rnn_layer.bidirectional else 1)
         self.dense = torch.nn.Linear(self.hidden_size, vocab_size)
+        self.state = None
 
     def forward(self, inputs, state):
         """
-        inputs: (batch_size, num_steps)
-        X:      (num_steps, batch_size, vocab_size)
+        inputs shape: (batch_size, num_steps)
+        X shape:      num_steps x (batch_size, vocab_size)
         hiddens:
         """
-        X = to_onehot(inputs, self.vocab_size)      # num_steps x (batch_size, vocab_size)
+        X = to_onehot(inputs, self.vocab_size)
         X = torch.stack(X)
         hiddens, state = self.rnn(X, state)
-        # hiddens:  (num_steps, batch_size, hidden_size)
-        # state:    (        1, batch_size, hidden_size)
         hiddens = hiddens.view(-1, hiddens.shape[-1])    # (num_steps*batch_size, hidden_size)
         output = self.dense(hiddens)
         return output, state
 
 
-def predict_rnn(prefix, num_chars, model, vocab_size, device, idx_to_char, char_to_idx):
+def predict_rnn(prefix, num_chars, model, idx_to_char, char_to_idx, device=None):
+    """
+    batch_size = 1
+    num_steps  = 1
+    """
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
     state = None
-    output = [char_to_idx[prefix[0]]]  # output记录prefix加上预测的num_chars个字符
+    output = [char_to_idx[prefix[0]]]
     for t in range(num_chars + len(prefix) - 1):
-        X = torch.tensor([output[-1]], device=device).view(1, 1)
-        (Y, state) = model(X, state)  # 前向计算不需要传入模型参数
+        X = torch.tensor([output[-1]], device=device).view(1,1)
+        if state is not None:
+            if isinstance(state, tuple):  # LSTM, state:(h, c)
+                state = (state[0].to(device), state[1].to(device))
+            else:
+                state = state.to(device)
+        (Y, state) = model(X, state)
         if t < len(prefix) - 1:
             output.append(char_to_idx[prefix[t + 1]])
         else:
@@ -94,28 +134,30 @@ def predict_rnn(prefix, num_chars, model, vocab_size, device, idx_to_char, char_
     return ''.join([idx_to_char[i] for i in output])
 
 
-def train_and_predict_rnn(model, num_hiddens, vocab_size, device,
-                                  corpus_indices, idx_to_char, char_to_idx,
-                                  num_epochs, num_steps, lr, clipping_theta,
-                                  batch_size, pred_period, pred_len, prefixes):
+
+def train_and_predict_rnn(model, corpus_indices, idx_to_char, char_to_idx,
+                          num_steps, batch_size,
+                          num_epochs, lr, clipping_theta,
+                          pred_period, pred_len, prefixes, device=None):
     loss = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     model.to(device)
+    epoch_perplexity, epoch_time = [], []
     for epoch in range(num_epochs):
         l_sum, n, start = 0.0, 0, time.time()
         data_iter = data_iter_consecutive(corpus_indices, batch_size, num_steps, device)  # 相邻采样
         state = None
         for X, Y in data_iter:
             if state is not None:
-                # 使用detach函数从计算图分离隐藏状态
-                if isinstance(state, tuple):  # LSTM, state:(h, c)
+                # 相邻采样使用detach函数从计算图分离隐藏状态
+                if isinstance(state, tuple):
                     state[0].detach_()
                     state[1].detach_()
                 else:
                     state.detach_()
-            (output, state) = model(X, state)  # output.shape: (num_steps*batch_size, vocab_size)
+            (output, state) = model(X, state)  # output.shape: (num_steps * batch_size, vocab_size)
             y = torch.flatten(Y.T)             # Y.shape:      (batch_size, num_steps)
-            l = loss(output, y.long())         # y.shape:      (batch_size*num_steps, 1)
+            l = loss(output, y.long())         # y.shape:      (num_steps * batch_size, 1)
 
             optimizer.zero_grad()
             l.backward()
@@ -125,24 +167,157 @@ def train_and_predict_rnn(model, num_hiddens, vocab_size, device,
             n += y.shape[0]
 
         if (epoch + 1) % pred_period == 0:
-            print('epoch %d, perplexity %f, time %.2f sec' % (
-                epoch + 1, math.exp(l_sum / n), time.time() - start))
+            p = math.exp(l_sum / n)
+            t = time.time() - start
+            epoch_perplexity.append(p)
+            epoch_time.append(t)
+            print('epoch %d, perplexity %f, time %.2f sec' % (epoch + 1, p, t))
             for prefix in prefixes:
-                print(' -', predict_rnn(
-                    prefix, pred_len, model, vocab_size, device, idx_to_char,
-                    char_to_idx))
+                print(' -', predict_rnn(prefix, pred_len, model, idx_to_char, char_to_idx, device))
+    return epoch_perplexity, epoch_time
+
+
+def plot_perplexity_and_time(perplexity, trainTime, legends, title):
+    """
+    superPameters, batch, metics
+    """
+    plt.figure(figsize=(12,6))
+    plt.subplot(121)
+    for batch_perplexity in perplexity:
+        plt.plot([50,100,150,200,250], batch_perplexity)
+    plt.legend(legends)
+    plt.xlabel("batch")
+    plt.ylabel("perplexity")
+    plt.title(title)
+    plt.subplot(122)
+    for batch_trainTime in trainTime:
+        plt.plot([50, 100, 150, 200, 250], batch_trainTime)
+    plt.legend(legends)
+    plt.xlabel("batch")
+    plt.ylabel("trainTime")
+    plt.title(title)
+    plt.savefig(os.path.join("log", title+".png"))
+    plt.close()
 
 
 if __name__=="__main__":
-    # predict_rnn('分开', 10, model, vocab_size, device, idx_to_char, char_to_idx)
-    num_epochs, batch_size, lr, clipping_theta = 250, 32, 1e-3, 1e-2
-    pred_period, pred_len, prefixes = 50, 50, ['分开', '不分开']
-    num_hiddens = 256
-    for num_hiddens in range(24, 512, 12):
-        model = RNNModel(vocab_size).to(device)
-        print("="*20,"num_hiddens=",num_hiddens,"="*20)
-        train_and_predict_rnn(model, num_hiddens, vocab_size, device,
-                              corpus_indices, idx_to_char, char_to_idx,
-                              num_epochs, num_steps, lr, clipping_theta,
-                              batch_size, pred_period, pred_len, prefixes)
+    num_steps, batch_size = 35, 32
+    num_epochs, lr, clipping_theta = 250, 1e-3, 1e-2
+    pred_period, pred_len, prefixes = 50, 100, ['分开', '不分开']
 
+
+    # RNN - bidirectional效果不好
+    # num_hiddens = 256
+    # rnn_layer = torch.nn.RNN(input_size=vocab_size, hidden_size=num_hiddens, num_layers=2, bidirectional=True)
+    # model = RNNModel(rnn_layer).to(device)
+    # # predict_rnn('分开', 10, model, vocab_size, idx_to_char, char_to_idx, device)
+    # train_and_predict_rnn(model, corpus_indices, idx_to_char, char_to_idx,
+    #                       num_steps, batch_size,
+    #                       num_epochs, lr, clipping_theta,
+    #                       pred_period, pred_len, prefixes, device)
+
+
+    # RNN - num_hiddens
+    print("=" * 80)
+    print("RNN - num_hiddens")
+    print("=" * 80)
+    perplexity, trainTime = [], []
+    hidden_size_seq = [64, 128, 256, 512]
+    for num_hiddens in hidden_size_seq:
+        print("=" * 20, "num_hiddens = ", num_hiddens, "=" * 20)
+        rnn_layer = torch.nn.RNN(input_size=vocab_size, hidden_size=num_hiddens, num_layers=1, bidirectional=False)
+        model = RNNModel(rnn_layer).to(device)
+        epoch_perplexity, epoch_time = train_and_predict_rnn(model, corpus_indices, idx_to_char, char_to_idx,
+                                                             num_steps, batch_size,
+                                                             num_epochs, lr, clipping_theta,
+                                                             pred_period, pred_len, prefixes, device)
+        perplexity.append(epoch_perplexity)
+        trainTime.append(epoch_time)
+    plot_perplexity_and_time(perplexity, trainTime, ["hidden_size="+str(i) for i in hidden_size_seq], "RNN - num_hiddens")
+
+
+    # RNN - num_layers
+    print("=" * 80)
+    print("RNN - num_layers")
+    print("=" * 80)
+    perplexity, trainTime = [], []
+    num_layers_seq = [1, 2, 3]
+    for num_layer in num_layers_seq:
+        print("=" * 20, "num_layer = ", num_layer, "=" * 20)
+        rnn_layer = torch.nn.RNN(input_size=vocab_size, hidden_size=256, num_layers=num_layer, bidirectional=False)
+        model = RNNModel(rnn_layer).to(device)
+        train_and_predict_rnn(model, corpus_indices, idx_to_char, char_to_idx,
+                              num_steps, batch_size,
+                              num_epochs, lr, clipping_theta,
+                              pred_period, pred_len, prefixes, device)
+    plot_perplexity_and_time(perplexity, trainTime, ["num_layers=" + str(i) for i in num_layers_seq], "RNN - num_layers")
+
+
+    # GRU - num_hiddens
+    print("=" * 80)
+    print("GRU - num_hiddens")
+    print("=" * 80)
+    perplexity, trainTime = [], []
+    hidden_size_seq = [64, 128, 256, 512]
+    for num_hiddens in hidden_size_seq:
+        print("=" * 20, "num_hiddens = ", num_hiddens, "=" * 20)
+        rnn_layer = torch.nn.GRU(input_size=vocab_size, hidden_size=num_hiddens, num_layers=1, bidirectional=False)
+        model = RNNModel(rnn_layer).to(device)
+        train_and_predict_rnn(model, corpus_indices, idx_to_char, char_to_idx,
+                              num_steps, batch_size,
+                              num_epochs, lr, clipping_theta,
+                              pred_period, pred_len, prefixes, device)
+    plot_perplexity_and_time(perplexity, trainTime, ["hidden_size="+str(i) for i in hidden_size_seq], "GRU - num_hiddens")
+
+
+    # GRU - num_layers
+    print("=" * 80)
+    print("GRU - num_layers")
+    print("=" * 80)
+    perplexity, trainTime = [], []
+    num_layers_seq = [1, 2, 3]
+    for num_layer in num_layers_seq:
+        print("=" * 20, "num_layer = ", num_layer, "=" * 20)
+        rnn_layer = torch.nn.GRU(input_size=vocab_size, hidden_size=256, num_layers=num_layer,
+                                 bidirectional=False)
+        model = RNNModel(rnn_layer).to(device)
+        train_and_predict_rnn(model, corpus_indices, idx_to_char, char_to_idx,
+                              num_steps, batch_size,
+                              num_epochs, lr, clipping_theta,
+                              pred_period, pred_len, prefixes, device)
+    plot_perplexity_and_time(perplexity, trainTime, ["num_layers=" + str(i) for i in num_layers_seq], "GRU - num_layers")
+
+
+    # LSTM - num_hiddens
+    print("=" * 80)
+    print("LSTM - num_hiddens")
+    print("=" * 80)
+    perplexity, trainTime = [], []
+    hidden_size_seq = [64, 128, 256, 512]
+    for num_hiddens in hidden_size_seq:
+        print("=" * 20, "num_hiddens = ", num_hiddens, "=" * 20)
+        rnn_layer = torch.nn.LSTM(input_size=vocab_size, hidden_size=num_hiddens, num_layers=1, bidirectional=False)
+        model = RNNModel(rnn_layer).to(device)
+        train_and_predict_rnn(model, corpus_indices, idx_to_char, char_to_idx,
+                              num_steps, batch_size,
+                              num_epochs, lr, clipping_theta,
+                              pred_period, pred_len, prefixes, device)
+    plot_perplexity_and_time(perplexity, trainTime, ["hidden_size=" + str(i) for i in hidden_size_seq], "LSTM - num_hiddens")
+
+
+    # LSTM - num_layers
+    print("=" * 80)
+    print("LSTM - num_layers")
+    print("=" * 80)
+    perplexity, trainTime = [], []
+    num_layers_seq = [1, 2, 3]
+    for num_layer in num_layers_seq:
+        print("=" * 20, "num_layer = ", num_layer, "=" * 20)
+        rnn_layer = torch.nn.LSTM(input_size=vocab_size, hidden_size=256, num_layers=num_layer,
+                                 bidirectional=False)
+        model = RNNModel(rnn_layer).to(device)
+        train_and_predict_rnn(model, corpus_indices, idx_to_char, char_to_idx,
+                              num_steps, batch_size,
+                              num_epochs, lr, clipping_theta,
+                              pred_period, pred_len, prefixes, device)
+    plot_perplexity_and_time(perplexity, trainTime, ["num_layers=" + str(i) for i in num_layers_seq], "LSTM - num_layers")
