7a63d69c94bf391ba5c94df193b761c71694648e
bamboosjtu <bamboopengkezhu@gmail.com>
2020-02-28 20:14:03 +0800

    开放题三

--- sru.py
+++ sru.py
@@ -4,6 +4,7 @@ import torch
 import numpy as np
 import utils
 
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 with open("Datasets/jaychou_lyrics.txt", encoding="utf-8") as f:
     corpus_chars = f.read()
@@ -15,8 +16,25 @@ corpus_indices = [char_to_idx[char] for char in corpus_chars]
 
 
 num_inputs, num_hiddens, num_outputs = vocab_size, 256, vocab_size
+def get_sru_params():
+    def _one(shape):
+        ts = torch.tensor(np.random.normal(0, 0.01, size=shape), device=device, dtype=torch.float32)
+        return torch.nn.Parameter(ts, requires_grad=True)
+    def _two():
+        return (_one((num_inputs, num_hiddens)),
+                torch.nn.Parameter(torch.zeros(num_hiddens, device=device, dtype=torch.float32), requires_grad=True))
 
-def get_params():
+    Wx = _one((num_inputs, num_hiddens))
+    W_xf, b_f = _two()  # 遗忘门参数
+    W_xr, b_r = _two()  # 重置门参数
+
+    # 输出层参数
+    W_hq = _one((num_hiddens, num_outputs))
+    b_q = torch.nn.Parameter(torch.zeros(num_outputs, device=device, dtype=torch.float32), requires_grad=True)
+    return torch.nn.ParameterList([Wx, W_xf, b_f, W_xr,b_r, W_hq, b_q])
+
+
+def get_gru_params():
     def _one(shape):
         ts = torch.tensor(np.random.normal(0, 0.01, size=shape), device=device, dtype=torch.float32)
         return torch.nn.Parameter(ts, requires_grad=True)
@@ -35,37 +53,26 @@ def get_params():
     return torch.nn.ParameterList([W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h, W_hq, b_q])
 
 
-def get_sru_params():
-    def _one(shape):
-        ts = torch.tensor(np.random.normal(0, 0.01, size=shape), device=device, dtype=torch.float32)
-        return torch.nn.Parameter(ts, requires_grad=True)
-    def _three():
-        return (_one((num_inputs, num_hiddens)),
-                _one((num_hiddens, num_hiddens)),
-                torch.nn.Parameter(torch.zeros(num_hiddens, device=device, dtype=torch.float32), requires_grad=True))
-    def _two():
-        return (_one((num_inputs, num_hiddens)),
-                torch.nn.Parameter(torch.zeros(num_hiddens, device=device, dtype=torch.float32), requires_grad=True))
-    W = _one((num_inputs, num_hiddens))
-    W_xf, b_f = _two()  # 遗忘门参数
-    W_xr, b_r = _two()  # 重置门参数
-    W_xh, W_hh, b_h = _three()  # 候选隐藏状态参数
-
-    # 输出层参数
-    W_hq = _one((num_hiddens, num_outputs))
-    b_q = torch.nn.Parameter(torch.zeros(num_outputs, device=device, dtype=torch.float32), requires_grad=True)
-    return torch.nn.ParameterList([W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h, W_hq, b_q])
-
-
 def init_gru_state(batch_size, num_hiddens, device):
     return (torch.zeros((batch_size, num_hiddens), device=device), )
 
 
+def sru(inputs, state, params):
+    Wx, W_xf, b_f, W_xr, b_r, W_hq, b_q = params
+    C, = state
+    outputs = []
+    for X in inputs:
+        X_tilda = torch.matmul(X, Wx)
+        F = torch.sigmoid(torch.matmul(X, W_xf) + b_f)
+        R = torch.sigmoid(torch.matmul(X, W_xr) + b_r)
+        C = F * C + (1-F) * X_tilda
+        H = R * torch.tanh(C) + (1-R) * X_tilda
+        Y = torch.matmul(H, W_hq) + b_q
+        outputs.append(Y)
+    return outputs, (C,)
+
+
 def gru(inputs, state, params):
-    """
-    input shape : numsteps x (batch_size, input_size)
-    output shape: numsteps x (batch_size, output_size)
-    """
     W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h, W_hq, b_q = params
     H, = state
     outputs = []
@@ -79,19 +86,23 @@ def gru(inputs, state, params):
     return outputs, (H,)
 
 
-num_epochs, num_steps, batch_size, lr, clipping_theta = 160, 35, 32, 1e2, 1e-2
 pred_period, pred_len, prefixes = 40, 50, ['分开', '不分开']
+num_epochs, num_steps, batch_size, lr, clipping_theta = 160, 35, 32, 1e2, 1e-2
 
-utils.train_and_predict_rnn(gru, get_params, init_gru_state, num_hiddens,
+print("="*40)
+print("using sru")
+print("="*40)
+utils.train_and_predict_rnn(sru, get_sru_params, init_gru_state, num_hiddens,
+                          vocab_size, device, corpus_indices, idx_to_char,
+                          char_to_idx, False, num_epochs, num_steps, lr,
+                          clipping_theta, batch_size, pred_period, pred_len,
+                          prefixes)
+print("="*40)
+print("using gru")
+print("="*40)
+utils.train_and_predict_rnn(gru, get_gru_params, init_gru_state, num_hiddens,
                           vocab_size, device, corpus_indices, idx_to_char,
                           char_to_idx, False, num_epochs, num_steps, lr,
                           clipping_theta, batch_size, pred_period, pred_len,
                           prefixes)
 
-lr = 1e-2 # 注意调整学习率
-gru_layer = torch.nn.GRU(input_size=vocab_size, hidden_size=num_hiddens)
-model = utils.RNNModel(gru_layer, vocab_size).to(device)
-utils.train_and_predict_rnn_pytorch(model, num_hiddens, vocab_size, device,
-                                corpus_indices, idx_to_char, char_to_idx,
-                                num_epochs, num_steps, lr, clipping_theta,
-                                batch_size, pred_period, pred_len, prefixes)
